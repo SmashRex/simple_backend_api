7dfb8c1f119940f8f99db6210c083f8b34739123
Temiloluwa <afolabitemiloluwa@gmail.com>
2025-02-04 07:07:28 +0100

    fixed the arrangement of the response

--- backend-api/src/main/java/com/hng/backend_api/controllers/ApiController.java
+++ backend-api/src/main/java/com/hng/backend_api/controllers/ApiController.java
@@ -7,25 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/")
-
-
 public class ApiController {
     @GetMapping
     public Map<String, String> getInfo() {
-        Map<String, String> response = new HashMap<>();
-        response.put("email", "afolabitemiloluwa4@gmail.com");
+        Map<String, String> response = new LinkedHashMap<>(); // Preserves key order
 
-        String currentDatetime = Instant.now().atOffset(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
-
-        response.put("current_datetime", currentDatetime);
-
-        response.put("github url", "https://github.com/SmashRex/simple_backend_api.git");
-
+        response.put("email", "afolabitemiloluwa4@gmail.com"); // First
+        response.put("current_datetime", Instant.now().atOffset(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT)); // Second
+        response.put("github_url", "https://github.com/SmashRex/simple_backend_api.git"); // Third
 
         return response;
     }
